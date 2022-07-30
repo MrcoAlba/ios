0bcab6029af12cc2153022b14746e56a5c4ce24a
mrcoalba <mrcoalba@gmail.com>
2022-07-30 15:01:49 -0500

    02. The I am Poor App

---
+++ 02.TheIAmPoorApp/I am Poor/I am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by Mrco on 27/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
